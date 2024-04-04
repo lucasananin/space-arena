f0801a3e91836753cd2f508f73b975a52cc11919
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-04-03 21:24:36 -0300

    feat: a* pathfinding and enemy follow test added.

---
+++ Space_Arena_Project/Assets/Scripts/_Tests/AiFollowTest.cs
@@ -0,0 +1,17 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiFollowTest : AIPath
+{
+    [SerializeField] Seeker _seeker = null;
+    [SerializeField] AIPath _aiPath = null;
+    [SerializeField] AIDestinationSetter _aIDestinationSetter = null;
+
+    public override void OnTargetReached()
+    {
+        base.OnTargetReached();
+        Debug.Log($"// TargetReached");
+    }
+}
