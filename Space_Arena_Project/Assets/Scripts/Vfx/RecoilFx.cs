8ccfe6cab07579fbe1d4568c98a8402130214828
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-04-20 20:34:40 -0300

    feat: recoil fx script adjusted.

--- Space_Arena_Project/Assets/Scripts/Vfx/RecoilFx.cs
+++ Space_Arena_Project/Assets/Scripts/Vfx/RecoilFx.cs
@@ -2,17 +2,13 @@ using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class RecoilFx : MonoBehaviour
 {
     [SerializeField] WeaponBehaviour _weaponBehaviour = null;
     [SerializeField] Transform _targetTransform = null;
-    [SerializeField] Animator _animator = null;
-    [SerializeField] Vector3 _offsetPosition = default;
-    [SerializeField] float _xOffset = 0.2f;
-    [SerializeField] float _recoilMultiplier = 1f;
-    [SerializeField] float _timeMultiplier = 1f;
+    [SerializeField] Vector3 _recoilOffset = new Vector3(-0.2f, 0f, 0f);
+    [SerializeField] float _timeMultiplier = 5f;
     [Space]
     [SerializeField, ReadOnly] Vector3 _defaultPosition = default;
     [SerializeField, ReadOnly] Vector3 _recoilPosition = default;
@@ -42,23 +38,7 @@ public class RecoilFx : MonoBehaviour
 
     private void _weapon_onShoot()
     {
-        //Vector3 _weaponBackDirection = -_weaponBehaviour.transform.right.normalized;
-        Vector3 _weaponBackDirection = -_targetTransform.right;
-        Vector3 _localBackPosition = _targetTransform.InverseTransformPoint(_weaponBackDirection);
-
-        _recoilPosition = (_localBackPosition * _recoilMultiplier) + _defaultPosition;
-        _recoilPosition.y = _defaultPosition.y;
-
-        //_targetTransform.localPosition = _recoilPosition;
-        //_targetTransform.DOComplete();
-        //_targetTransform.DOLocalMove(_defaultPosition, _recoilMultiplier);
-
-        //_animator.SetTrigger("Shoot");
-
-        //_recoilPosition = _offsetPosition;
-        _recoilPosition = _defaultPosition;
-        _recoilPosition.x -= _recoilMultiplier;
-
+        _recoilPosition = _defaultPosition + _recoilOffset;
         _timer = 0f;
     }
 }
