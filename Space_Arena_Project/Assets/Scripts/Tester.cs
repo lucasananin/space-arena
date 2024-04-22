7597e4f389f5a55277befcfd975eeaa804c612ac
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-04-22 18:13:49 -0300

    feat: enemy movement build.

---
+++ Space_Arena_Project/Assets/Scripts/Tester.cs
@@ -0,0 +1,32 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    [SerializeField] AIPath[] _enemies = null;
+
+    private void Start()
+    {
+        ChangeEnemiesMoveState();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ChangeEnemiesMoveState();
+        }
+    }
+
+    private void ChangeEnemiesMoveState()
+    {
+        int _count = _enemies.Length;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _enemies[i].canMove = !_enemies[i].canMove;
+        }
+    }
+}
