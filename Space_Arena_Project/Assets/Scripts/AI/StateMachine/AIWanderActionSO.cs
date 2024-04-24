9bd0586c3783c7aa03a1af3bf2e780cc076fc97e
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-04-24 17:02:42 -0300

    feat: ai wander action so script cleaned.

--- Space_Arena_Project/Assets/Scripts/AI/StateMachine/AIWanderActionSO.cs
+++ Space_Arena_Project/Assets/Scripts/AI/StateMachine/AIWanderActionSO.cs
@@ -10,7 +10,6 @@ public class AIWanderActionSO : StateActionSO
 {
     [SerializeField] float _radius = 5f;
 
-    //public float Radius => _radius;
     public float Radius { get => _radius; private set => _radius = value; }
 
     protected override StateAction CreateAction()
@@ -23,36 +22,12 @@ public class AIWanderAction : StateAction
 {
     private new AIWanderActionSO OriginSO => (AIWanderActionSO)base.OriginSO;
 
-    //private StateMachine _stateMachine = null;
     private IAstarAI _aiPath = default;
-    //private float _radius = 5;
-
-    //private GameObject _player = null;
-    //private GridGraph _graph = null;
-    //private int _graphIndex = 0;
 
     public override void Awake(StateMachine _stateMachine)
     {
-        //this._stateMachine = _stateMachine;
         _aiPath = _stateMachine.GetComponent<AIPath>();
-
-        //_graph = AstarPath.active.data.graphs[_graphIndex] as GridGraph;
-        //_player = GameObject.FindGameObjectWithTag("Player");
     }
-
-    //public override void OnStateEnter()
-    //{
-    //    Vector3 _size = new Vector3(5f, 5f, 0f);
-    //    Bounds _bounds = new Bounds(_stateMachine.transform.position, _size);
-    //    List<GraphNode> _nodes = _graph.GetNodesInRegion(_bounds);
-
-    //    int _count = _nodes.Count;
-
-    //    for (int i = 0; i < _count; i++)
-    //    {
-    //        Debug.Log($"// _node.position = {_nodes[i].RandomPointOnSurface()}");
-    //    }
-    //}
 
     public override void OnFixedUpdate()
     {
@@ -76,7 +51,6 @@ public class AIWanderAction : StateAction
     {
         Vector3 _point = Random.insideUnitSphere * OriginSO.Radius;
         _point.z = 0;
-        //_point += _player.transform.position;
         _point += _aiPath.position;
         return _point;
     }
