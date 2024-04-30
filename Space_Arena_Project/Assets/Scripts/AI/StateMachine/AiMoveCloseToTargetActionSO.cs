cecb429be9f1841d1c404092f6f83a659a61063c
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-04-30 16:10:01 -0300

    feat: move close to target properties added to the ai entity so.

--- Space_Arena_Project/Assets/Scripts/AI/StateMachine/AiMoveCloseToTargetActionSO.cs
+++ Space_Arena_Project/Assets/Scripts/AI/StateMachine/AiMoveCloseToTargetActionSO.cs
@@ -7,13 +7,11 @@ using UOP1.StateMachine.ScriptableObjects;
 [CreateAssetMenu(fileName = "Action_Ai_MoveCloserToTarget", menuName = "SO/State Machines/Actions/AI Move Closer To Target")]
 public class AiMoveCloseToTargetActionSO : StateActionSO
 {
-    [SerializeField] float _radius = 1f;
-    [SerializeField] float _moveRate = 1f;
-    [SerializeField] bool _stopOnCloseEnough = true;
+    //[SerializeField] float _radius = 1f;
+    //[SerializeField] bool _stopOnCloseEnough = true;
 
-    public float Radius { get => _radius; private set => _radius = value; }
-    public float MoveRate { get => _moveRate; private set => _moveRate = value; }
-    public bool StopOnCloseEnough { get => _stopOnCloseEnough; private set => _stopOnCloseEnough = value; }
+    //public float Radius { get => _radius; private set => _radius = value; }
+    //public bool StopOnCloseEnough { get => _stopOnCloseEnough; private set => _stopOnCloseEnough = value; }
 
     protected override StateAction CreateAction()
     {
@@ -23,14 +21,16 @@ public class AiMoveCloseToTargetActionSO : StateActionSO
 
 public class AiMoveCloseToTargetAction : StateAction
 {
-    private new AiMoveCloseToTargetActionSO OriginSO => (AiMoveCloseToTargetActionSO)base.OriginSO;
+    //private new AiMoveCloseToTargetActionSO OriginSO => (AiMoveCloseToTargetActionSO)base.OriginSO;
 
     private AiEntity _aIEntity = null;
+    private AiEntitySO _entitySO = null;
     private Vector3 _point = default;
 
     public override void Awake(StateMachine stateMachine)
     {
         _aIEntity = stateMachine.GetComponent<AiEntity>();
+        _entitySO = _aIEntity.GetEntitySO<AiEntitySO>();
     }
 
     public override void OnStateEnter()
@@ -45,9 +45,9 @@ public class AiMoveCloseToTargetAction : StateAction
 
     public override void OnUpdate()
     {
-        if (OriginSO.StopOnCloseEnough && _aIEntity.IsCloseToTargetEntity(OriginSO.Radius)) return;
+        if (_entitySO.StopOnCloseEnough && _aIEntity.IsCloseToTargetEntity(_entitySO.MoveClose_radius)) return;
 
-        if (_aIEntity.HasReachedPathEnding() || !_aIEntity.IsPointCloseToTargetEntity(_point, OriginSO.Radius))
+        if (_aIEntity.HasReachedPathEnding() || !_aIEntity.IsPointCloseToTargetEntity(_point, _entitySO.MoveClose_radius))
         {
             SearchPath();
         }
@@ -57,7 +57,7 @@ public class AiMoveCloseToTargetAction : StateAction
     {
         // Pega uma posicao onde o target possa ser visto. Castar um raycast pra ver se nao tem nada na frente.
         // Colocar um numero maximo de tentativas para evitar lacos infinitos.
-        _point = _aIEntity.PickRandomPointNearTarget(OriginSO.Radius);
+        _point = _aIEntity.PickRandomPointNearTarget(_entitySO.MoveClose_radius);
         _aIEntity.SetAIPathDestination(_point);
     }
 }
--- Space_Arena_Project/Assets/Scripts/Entities/AiEntitySO.cs
+++ Space_Arena_Project/Assets/Scripts/Entities/AiEntitySO.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Entity_", menuName = "SO/Entities/AI Entity")]
 public class AiEntitySO : EntitySO
 {
+    [Title("// Shoot Target")]
     [SerializeField, Range(0f, 99f)] float _shootDistance = 0f;
 
+    [Title("// Move Close To Target")]
+    [SerializeField] float _moveClose_radius = 0f;
+    [SerializeField] bool _stopOnCloseEnough = true;
+
     public float ShootDistance { get => _shootDistance; private set => _shootDistance = value; }
+
+    public float MoveClose_radius { get => _moveClose_radius; private set => _moveClose_radius = value; }
+    public bool StopOnCloseEnough { get => _stopOnCloseEnough; set => _stopOnCloseEnough = value; }
 }
