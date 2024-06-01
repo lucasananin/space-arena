fd2fb6ee08d480ab14b5f63ffa9182009868e13f
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-01 18:17:11 -0300

    style: weapon rotator code cleaned.

--- Space_Arena_Project/Assets/Scripts/Weapon/WeaponRotator.cs
+++ Space_Arena_Project/Assets/Scripts/Weapon/WeaponRotator.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class WeaponRotator : MonoBehaviour
 {
     [SerializeField] bool _canRotate = true;
-    //[SerializeField] bool _smoothRotation = false;
-    //[SerializeField] float _rotationSpeed = 0f;
 
     public void LookAtMouse()
     {
         if (!CanRotate()) return;
 
-        //Vector3 _mousePosition = InputHandler.GetMousePosition();
-        //Vector3 _direction = _mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        //float _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-        //Quaternion _rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
         var _target = Camera.main.ScreenToWorldPoint(InputHandler.GetMousePosition());
         var _rotation = GeneralMethods.GetLookRotation(transform.position, _target);
         SetRotation(_rotation);
@@ -25,21 +19,9 @@ public class WeaponRotator : MonoBehaviour
     {
         if (!CanRotate()) return;
 
-        //Vector3 _direction = _targetPosition - transform.position;
-        //float _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-        //Quaternion _rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
         var _rotation = GeneralMethods.GetLookRotation(transform.position, _position);
         SetRotation(_rotation);
     }
-
-    //public void LookAtDirection(Vector3 _direction)
-    //{
-    //    if (!CanRotate()) return;
-
-    //    float _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-    //    Quaternion _rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
-    //    SetRotation(_rotation);
-    //}
 
     public void ResetRotation()
     {
@@ -48,11 +30,6 @@ public class WeaponRotator : MonoBehaviour
 
     private void SetRotation(Quaternion _rotation)
     {
-        //if (_smoothRotation)
-        //    transform.rotation = Quaternion.Slerp(transform.rotation, _rotation, _rotationSpeed * Time.deltaTime);
-        //else
-        //    transform.rotation = _rotation;
-
         transform.rotation = _rotation;
     }
 
