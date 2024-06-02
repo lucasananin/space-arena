763b1f3ef8df877bd89814b55d839bf7419c9edd
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-02 12:52:57 -0300

    feat: rotate fx added to the molotov projectile.

---
+++ Space_Arena_Project/Assets/Scripts/Vfx/RotateFx.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateFx : MonoBehaviour
+{
+    [SerializeField] Transform _transform = null;
+    [SerializeField] float _multiplier = 1f;
+    [SerializeField] float _slowDownMultiplier = 1f;
+
+    private void Update()
+    {
+        _multiplier -= Time.deltaTime * _slowDownMultiplier;
+        var _euler = Vector3.forward * _multiplier;
+        _transform.Rotate(_euler);
+    }
+}
