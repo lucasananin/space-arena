91e854cff7c8a7e451a5edb4453d0ecfdb2a995e
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-05 22:51:30 -0300

    feat: guided projectile now works with AI too.

--- Space_Arena_Project/Assets/Scripts/Projectile/GuidedProjectile.cs
+++ Space_Arena_Project/Assets/Scripts/Projectile/GuidedProjectile.cs
@@ -16,14 +16,23 @@ public class GuidedProjectile : ProjectileBehaviour
     public override void Init(ShootModel _newShootModel)
     {
         base.Init(_newShootModel);
-
-        var _mouseWorldPosition = Camera.main.ScreenToWorldPoint(InputHandler.GetMousePosition());
-        var _randomPosition = Random.insideUnitCircle * _projectileSO.MaxPositionRadius;
-        var _finalPosition = (Vector2)_mouseWorldPosition + _randomPosition;
-        transform.position = _finalPosition;
-
+        transform.position = GeneratePosition(_newShootModel);
         _visualTransform.localScale = Vector3.one * _projectileSO.ExplosionRadius * 2f;
-
         SendInitEvent();
+    }
+
+    private Vector3 GeneratePosition(ShootModel _newShootModel)
+    {
+        if (_newShootModel.EntitySource is AiEntity _aiEntity)
+        {
+            var _randomPosition = Random.insideUnitCircle * _projectileSO.MaxPositionRadius;
+            return (Vector2)_aiEntity.GetTargetEntityPosition() + _randomPosition;
+        }
+        else
+        {
+            var _mouseWorldPosition = Camera.main.ScreenToWorldPoint(InputHandler.GetMousePosition());
+            var _randomPosition = Random.insideUnitCircle * _projectileSO.MaxPositionRadius;
+            return (Vector2)_mouseWorldPosition + _randomPosition;
+        }
     }
 }
