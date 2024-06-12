9918f0fe6adb486ff4fb601ca5b9a2d33b5b755e
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-12 20:57:45 -0300

    feat: shrinkfx script added to the castStrightLineVfx.

---
+++ Space_Arena_Project/Assets/Scripts/Vfx/ShrinkFx.cs
@@ -0,0 +1,45 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkFx : MonoBehaviour
+{
+    [SerializeField] Transform[] _transforms = null;
+    [SerializeField] float _shrinkTimeMultiplier = 1f;
+    [SerializeField, ReadOnly] List<Vector3> _defaultScales = null;
+    [SerializeField, ReadOnly] float _shrinkTimer = 0f;
+
+    private void Awake()
+    {
+        SetDefaults();
+    }
+
+    private void Update()
+    {
+        ShrinkRenderers();
+    }
+
+    private void ShrinkRenderers()
+    {
+        if (_shrinkTimer > 1) return;
+
+        _shrinkTimer += Time.deltaTime * _shrinkTimeMultiplier;
+        int _count = _transforms.Length;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _transforms[i].localScale = Vector3.Lerp(_defaultScales[i], Vector3.zero, _shrinkTimer);
+        }
+    }
+
+    private void SetDefaults()
+    {
+        int _count = _transforms.Length;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _defaultScales.Add(_transforms[i].localScale);
+        }
+    }
+}
