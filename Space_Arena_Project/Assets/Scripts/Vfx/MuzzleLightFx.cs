34ae39fb40a4001a9f72701aa2f09470d3e5652f
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-13 17:06:44 -0300

    style: muzzle light fx duration added.

--- Space_Arena_Project/Assets/Scripts/Vfx/MuzzleLightFx.cs
+++ Space_Arena_Project/Assets/Scripts/Vfx/MuzzleLightFx.cs
@@ -9,14 +9,15 @@ public class MuzzleLightFx : MonoBehaviour
     [SerializeField] WeaponBehaviour _weapon = null;
     [SerializeField] Light2D _light2D = null;
     [Space]
-    [SerializeField] float _minIntensity = 3f;
-    [SerializeField] float _maxIntensity = 5f;
+    [SerializeField] float _minIntensity = 4f;
+    [SerializeField] float _maxIntensity = 6f;
     [SerializeField] float _minRadiusMultiplier = 1f;
     [SerializeField] float _maxRadiusMultiplier = 2f;
     [Space]
-    [SerializeField] float _timeEnabled = 0.05f;
+    [SerializeField] float _duration = 0.08f;
     [Space]
     [SerializeField, ReadOnly] float _timer = 0f;
+    [SerializeField, ReadOnly] float _initialIntensity = 0f;
     [SerializeField, ReadOnly] float _defaultInnerRadius = 0f;
     [SerializeField, ReadOnly] float _defaultOuterRadius = 0f;
 
@@ -29,35 +30,30 @@ public class MuzzleLightFx : MonoBehaviour
 
     private void OnEnable()
     {
-        _weapon.onShoot += Show;
+        _weapon.onShoot += Init;
     }
 
     private void OnDisable()
     {
-        _weapon.onShoot -= Show;
+        _weapon.onShoot -= Init;
     }
 
     private void Update()
     {
-        if (!_light2D.enabled) return;
+        if (_timer > 1) return;
 
-        _timer += Time.deltaTime;
-
-        if (_timer >= _timeEnabled)
-        {
-            _light2D.enabled = false;
-        }
+        _timer += Time.deltaTime * (1f / _duration);
+        _light2D.intensity = Mathf.Lerp(_initialIntensity, 0f, _timer);
     }
 
-    public void Show()
+    public void Init()
     {
+        _timer = 0f;
         _light2D.enabled = true;
-        _light2D.intensity = Random.Range(_minIntensity, _maxIntensity);
+        _initialIntensity = Random.Range(_minIntensity, _maxIntensity);
 
         float _randomRadiusMultiplier = Random.Range(_minRadiusMultiplier, _maxRadiusMultiplier);
         _light2D.pointLightInnerRadius = _defaultInnerRadius * _randomRadiusMultiplier;
         _light2D.pointLightOuterRadius = _defaultOuterRadius * _randomRadiusMultiplier;
-
-        _timer = 0f;
     }
 }
