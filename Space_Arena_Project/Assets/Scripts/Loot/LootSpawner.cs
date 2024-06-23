7e973adb27aa327a566028a028a9eb6f1ec32236
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-23 20:08:45 -0300

    feat: spawn chest on round end.

--- Space_Arena_Project/Assets/Scripts/Loot/LootSpawner.cs
+++ Space_Arena_Project/Assets/Scripts/Loot/LootSpawner.cs
@@ -6,15 +6,24 @@ using UnityEngine;
 
 public class LootSpawner : MonoBehaviour
 {
+    [Title("// General")]
     [SerializeField] Transform _parentContainer = null;
-    [SerializeField] Vector2 _minMaxForce = default;
-    [Space]
-    [SerializeField] WeaponSO[] _weaponSOs = null;
+
+    [Title("// Weapons")]
+    [SerializeField] bool _spawnOnStart = false;
     [SerializeField] LootBehaviour _weaponLootPrefab = null;
+    [SerializeField] BoxCollider2D _weaponCollider = null;
+    [SerializeField] WeaponSO[] _weapons = null;
+
+    [Title("// Collectables")]
     [SerializeField] CollectableBehaviour _ammoPrefab = null;
     [SerializeField] CollectableBehaviour _healthPrefab = null;
-    [SerializeField] BoxCollider2D _boxCollider2D = null;
-    [SerializeField] bool _spawnOnStart = false;
+    [SerializeField] Vector2 _minMaxForce = default;
+
+    [Title("// Chests")]
+    [SerializeField] WeaponChestCollidable _chestPrefab = null;
+    [SerializeField] BoxCollider2D _chestCollider = null;
+    [SerializeField, ReadOnly] WeaponChestCollidable _currentChest = null;
 
     //private GridGraph _graph = null;
 
@@ -28,12 +37,21 @@ public class LootSpawner : MonoBehaviour
         }
     }
 
+    private void OnEnable()
+    {
+        EnemySpawner.onEnd += SpawnChest;
+        EnemySpawner.onStart += DestroyChest;
+    }
+
+    private void OnDisable()
+    {
+        EnemySpawner.onEnd -= SpawnChest;
+        EnemySpawner.onStart -= DestroyChest;
+    }
+
     [Button]
     private void SpawnRandomWeapon()
     {
-        Vector3 _position = GeneralMethods.GetRandomPointInBounds(_boxCollider2D.bounds);
-        LootBehaviour _loot = Instantiate(_weaponLootPrefab, _position, Quaternion.identity, _parentContainer);
-
         //GridNode[] _nodes = _graph.nodes;
         //int _randomNodeIndex = Random.Range(0, _nodes.Length);
         //Vector3 _position = (Vector3)_nodes[_randomNodeIndex].position;
@@ -42,30 +60,45 @@ public class LootSpawner : MonoBehaviour
         //Vector3 _randomPosition = Random.insideUnitCircle * 5f;
         //LootBehaviour _loot = Instantiate(_weaponLootPrefab, _randomPosition, Quaternion.identity, _parentContainer);
 
-        int _randomWeaponIndex = Random.Range(0, _weaponSOs.Length);
-        WeaponSO _so = _weaponSOs[_randomWeaponIndex];
+        var _position = GeneralMethods.GetRandomPointInBounds(_weaponCollider.bounds);
+        var _loot = Instantiate(_weaponLootPrefab, _position, Quaternion.identity, _parentContainer);
+        var _randomWeaponIndex = Random.Range(0, _weapons.Length);
+        var _so = _weapons[_randomWeaponIndex];
         _loot.Init(_so);
     }
 
-    [Button]
+    //[Button]
     private void SpawnEachWeaponOnList()
     {
-        int _count = _weaponSOs.Length;
+        int _count = _weapons.Length;
 
         for (int i = 0; i < _count; i++)
         {
-            Vector3 _position = GeneralMethods.GetRandomPointInBounds(_boxCollider2D.bounds);
-            LootBehaviour _loot = Instantiate(_weaponLootPrefab, _position, Quaternion.identity, _parentContainer);
-
-            WeaponSO _so = _weaponSOs[i];
+            var _position = GeneralMethods.GetRandomPointInBounds(_weaponCollider.bounds);
+            var _loot = Instantiate(_weaponLootPrefab, _position, Quaternion.identity, _parentContainer);
+            var _so = _weapons[i];
             _loot.Init(_so);
         }
     }
 
     [Button]
+    private void SpawnChest()
+    {
+        var _position = GeneralMethods.GetRandomPointInBounds(_chestCollider.bounds);
+        var _chest = Instantiate(_chestPrefab, _position, Quaternion.identity, _parentContainer);
+        _currentChest = _chest;
+    }
+
+    private void DestroyChest()
+    {
+        if (_currentChest is not null)
+            Destroy(_currentChest.gameObject);
+    }
+
+    //[Button]
     private void SpawnAmmoPack()
     {
-        var _position = GeneralMethods.GetRandomPointInBounds(_boxCollider2D.bounds);
+        var _position = GeneralMethods.GetRandomPointInBounds(_weaponCollider.bounds);
         var _randomEuler = new Vector3(0f, 0f, Random.rotation.eulerAngles.z);
         Instantiate(_ammoPrefab, _position, Quaternion.Euler(_randomEuler), _parentContainer);
         //var _loot = Instantiate(_ammoPrefab, _position, Quaternion.Euler(_randomEuler), _parentContainer);
@@ -74,10 +107,10 @@ public class LootSpawner : MonoBehaviour
         //_loot.Init(_so);
     }
 
-    [Button]
+    //[Button]
     private void SpawnHealtPack()
     {
-        var _position = GeneralMethods.GetRandomPointInBounds(_boxCollider2D.bounds);
+        var _position = GeneralMethods.GetRandomPointInBounds(_weaponCollider.bounds);
         Instantiate(_healthPrefab, _position, Quaternion.identity, _parentContainer);
     }
 
@@ -106,6 +139,7 @@ public class LootSpawner : MonoBehaviour
 
                     break;
                 default:
+                    Debug.LogError($"// This loot type SO is not available! {nameof(_lootSpawnInfo.so)}");
                     break;
             }
         }
