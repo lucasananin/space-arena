9aa91caa90dae934037f131784d3d50762b8ad41
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-06-28 17:27:29 -0300

    style: multi weapon handler script cleaned.

--- Space_Arena_Project/Assets/Scripts/AI/MultiWeaponHandler.cs
+++ Space_Arena_Project/Assets/Scripts/AI/MultiWeaponHandler.cs
@@ -8,11 +8,6 @@ public class MultiWeaponHandler : MonoBehaviour
     [SerializeField] EntityBehaviour _entity = null;
     [SerializeField] List<MultiWeaponModel> _multiWeapons = null;
 
-    // 1.
-    // conta um tempo para atirar pelo model.
-    // ao acabar o tempo, tenta atirar, se conseguir, inicia uma coroutine e ao fim dela, zera o tempo.
-    // se nao conseguir atirar, zera o tempo.
-    // o tempo pode ser um minMax.
     private void Awake()
     {
         int _count = _multiWeapons.Count;
@@ -33,11 +28,12 @@ public class MultiWeaponHandler : MonoBehaviour
         {
             var _m = _multiWeapons[i];
             _m.IncreaseTime();
+            // rotate
+            // flip
 
-            if (_m.CanShoot())
+            if (_m.HasEnoughFireTime())
             {
                 _m.ResetTime();
-                //_m.StartShooting();
                 var _weapon = _m.GetRandomWeapon();
                 StartCoroutine(Shoot_routine(_weapon));
             }
@@ -49,11 +45,11 @@ public class MultiWeaponHandler : MonoBehaviour
         _weapon.PullTrigger();
         yield return null;
         _weapon.ReleaseTrigger();
-        Debug.Log($"// {_weapon.name} has been shot!");
+        //Debug.Log($"// {_weapon.name} has been shot!");
     }
 
     // 2. (talvez seja melhor ignorar isso para facilitar o meu trabalho)
-    // * Talvez criar um tryMultiShootActionSO?
+    // * Talvez criar um tryMultiShootActionSO? OU talvez passar os valores como parametros.
     // checa se o alvo esta dentro da distancia minima.
     // checa se o alvo esta na mira.
 }
@@ -61,20 +57,12 @@ public class MultiWeaponHandler : MonoBehaviour
 [System.Serializable]
 public class MultiWeaponModel
 {
-    public string _id = null;
     [SerializeField] List<WeaponBehaviour> _weapons = null;
     [SerializeField] Vector2 _minMaxRate = default;
     [SerializeField, ReadOnly] float _fireRate = 0f;
     [SerializeField, ReadOnly] float _nextFire = 0f;
     [SerializeField, ReadOnly] List<WeaponRotator> _rotators = null;
     [SerializeField, ReadOnly] List<WeaponFlipper> _flippers = null;
-
-    public void ResetTime()
-    {
-        // if !canShoot return;
-        _fireRate = Random.Range(_minMaxRate.x, _minMaxRate.y);
-        _nextFire = 0;
-    }
 
     public void InitWeapons(EntityBehaviour _entitySource)
     {
@@ -86,29 +74,16 @@ public class MultiWeaponModel
         }
     }
 
+    public void ResetTime()
+    {
+        _fireRate = Random.Range(_minMaxRate.x, _minMaxRate.y);
+        _nextFire = 0;
+    }
+
     public void IncreaseTime()
     {
         _nextFire += Time.deltaTime;
     }
-
-    public void StartShooting()
-    {
-        ResetTime();
-        int _rand = Random.Range(0, _weapons.Count);
-        var _weapon = _weapons[_rand];
-        //_weapon.PullTrigger();
-        //_weapon.ReleaseTrigger();
-        Debug.Log($"// {_weapon.name} has been shot!");
-        //Debug.Log($"// {_id} shot weapon {_rand}!");
-        // StartShoot_routine();
-    }
-
-    //private IEnumerator Shoot_routine(WeaponBehaviour _weapon)
-    //{
-    //    _weapon.PullTrigger();
-    //    yield return null;
-    //    _weapon.ReleaseTrigger();
-    //}
 
     public WeaponBehaviour GetRandomWeapon()
     {
@@ -116,8 +91,22 @@ public class MultiWeaponModel
         return _weapons[_randomIndex];
     }
 
-    public bool CanShoot()
+    public bool HasEnoughFireTime()
     {
         return _nextFire > _fireRate;
+    }
+
+    [Button]
+    private void SetReferences()
+    {
+        int _count = _weapons.Count;
+        _rotators.Clear();
+        _flippers.Clear();
+
+        for (int i = 0; i < _count; i++)
+        {
+            _rotators.Add(_weapons[i].GetComponent<WeaponRotator>());
+            _flippers.Add(_weapons[i].GetComponent<WeaponFlipper>());
+        }
     }
 }
