68767ec3ca94b9cdd6419e1a2521ea1cf87efb5b
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-07-24 14:59:32 -0300

    style: mortar line spawner initial position adjustments.

--- Space_Arena_Project/Assets/Scripts/Vfx/Lines/MortarLineSpawner.cs
+++ Space_Arena_Project/Assets/Scripts/Vfx/Lines/MortarLineSpawner.cs
@@ -6,23 +6,23 @@ public class MortarLineSpawner : MonoBehaviour
 {
     [SerializeField] ProjectileBehaviour _projectile = null;
     [SerializeField] StraightLineVfx _lineVfx = null;
+    [SerializeField] Vector3 _offset = default;
 
     private void OnEnable()
     {
-        _projectile.OnDestroy_TimerEnd += _projectile_OnDestroy_TimerEnd;
+        _projectile.OnDestroy_TimerEnd += SpawnFx;
     }
 
     private void OnDisable()
     {
-        _projectile.OnDestroy_TimerEnd -= _projectile_OnDestroy_TimerEnd;
+        _projectile.OnDestroy_TimerEnd -= SpawnFx;
     }
 
-    private void _projectile_OnDestroy_TimerEnd()
+    private void SpawnFx()
     {
-        var _initialPosition = Vector2.up * 20f + Vector2.left * -2;
-        var _instance = Instantiate(_lineVfx, _initialPosition, transform.rotation);
-
-        var _finalPosition = transform.position;
-        _instance.Init(_finalPosition);
+        var _myPosition = transform.position;
+        var _startPosition = _myPosition + _offset;
+        var _instance = Instantiate(_lineVfx, _startPosition, transform.rotation);
+        _instance.Init(_myPosition);
     }
 }
