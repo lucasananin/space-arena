df5a4b0c257013742c47084f55f8cde1ab0ab1a0
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-10-31 18:16:25 -0300

    feat: self destruct sfx added.

--- Space_Arena_Project/Assets/Scripts/Vfx/SelfDestructFx.cs
+++ Space_Arena_Project/Assets/Scripts/Vfx/SelfDestructFx.cs
@@ -1,5 +1,4 @@
 using DG.Tweening;
-//using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,27 +6,17 @@ using UnityEngine;
 public class SelfDestructFx : MonoBehaviour
 {
     [SerializeField] Transform _renderTransform = null;
+    [SerializeField] AudioCue _sfx = null;
     [Space]
     [SerializeField] Vector3 _strength = new(1f, 1f, 0f);
     [SerializeField, Range(0.1f, 9f)] float _strenghtMultiplier = 1f;
     [SerializeField, Range(0.1f, 9f)] float _duration = 1f;
     [SerializeField, Range(1, 99)] int _vibrato = 10;
-    //[Space]
-    //[SerializeField, ReadOnly] Vector3 _defaultPosition = default;
-
-    //private void Awake()
-    //{
-    //    _defaultPosition = _renderTransform.localPosition;
-    //}
 
     public void Play()
     {
+        _sfx.PlayAudioCue();
         _renderTransform.DOComplete();
         _renderTransform.DOShakePosition(_duration, _strength * _strenghtMultiplier, _vibrato);
-        //_renderTransform.DOShakePosition(_duration, _strength * _strenghtMultiplier, _vibrato).
-        //    OnComplete(() =>
-        //    {
-        //        _renderTransform.localPosition = _defaultPosition;
-        //    });
     }
 }
