ad6301d705c1f8790033ebf0fc5dd940082cee39
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-11-01 21:57:53 -0300

    feat: sniper lock on sfx added.

--- Space_Arena_Project/Assets/Scripts/Vfx/AiSniperLineFx.cs
+++ Space_Arena_Project/Assets/Scripts/Vfx/AiSniperLineFx.cs
@@ -8,10 +8,12 @@ public class AiSniperLineFx : MonoBehaviour
     [SerializeField] AiEntity _aiEntity = null;
     [SerializeField] AiWeaponHandler _aiWeaponHandler = null;
     [SerializeField] LineRenderer _line = null;
+    [SerializeField] AudioCue _sfx = null;
     [SerializeField, Range(0f, 1f)] float _minTime = 0.5f;
     [SerializeField, Range(1f, 99f)] float _lineLength = 20f;
     [SerializeField] int _modelStartIndex = 0;
     [SerializeField, ReadOnly] AiWeaponModel _aiWeaponModel = null;
+    [SerializeField, ReadOnly] bool _wasEnabled = false;
 
     private void Awake()
     {
@@ -22,7 +24,17 @@ public class AiSniperLineFx : MonoBehaviour
 
     private void LateUpdate()
     {
+        _wasEnabled = _line.enabled;
         _line.enabled = _aiEntity.IsTargetOnLineOfSight && HasEnoughShootTime();
+
+        if (_line.enabled && !_wasEnabled)
+        {
+            _sfx.PlayAudioCue();
+        }
+        //else if (!_line.enabled && _wasEnabled)
+        //{
+        //    _sfx.StopAudioCue();
+        //}
     }
 
     public bool HasEnoughShootTime()
