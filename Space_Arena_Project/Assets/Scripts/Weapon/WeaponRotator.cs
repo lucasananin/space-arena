c35a139ac6b1c813174d94d385a354c5ab434c3c
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-12-21 20:42:34 -0300

    fix: guided_1 weapon overflow adjusted.

--- Space_Arena_Project/Assets/Scripts/Weapon/WeaponRotator.cs
+++ Space_Arena_Project/Assets/Scripts/Weapon/WeaponRotator.cs
@@ -10,33 +10,33 @@ public class WeaponRotator : MonoBehaviour
     {
         if (!CanRotate()) return;
 
-        var _target = Camera.main.ScreenToWorldPoint(InputHandler.GetMousePosition());
-        var _direction = transform.parent.InverseTransformPoint(_target);
-        var _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-        transform.localRotation = Quaternion.Euler(0f, 0f, _angle);
+        var _mouseWorldPosition = Camera.main.ScreenToWorldPoint(InputHandler.GetMousePosition());
+        LookAtPosition(_mouseWorldPosition);
+        //var _direction = transform.parent.InverseTransformPoint(_target);
+        //var _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        //transform.localRotation = Quaternion.Euler(0f, 0f, _angle);
     }
 
     public void LookAtPosition(Vector3 _position)
     {
         if (!CanRotate()) return;
 
-        var _target = _position;
-        var _direction = transform.parent.InverseTransformPoint(_target);
+        var _direction = transform.parent.InverseTransformPoint(_position);
         var _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
         transform.localRotation = Quaternion.Euler(0f, 0f, _angle);
     }
 
     public void ResetRotation()
     {
-        var _target = transform.position + new Vector3(transform.parent.right.x, 0, 0);
-        var _direction = transform.parent.InverseTransformPoint(_target);
+        var _parentForwardPosition = transform.position + new Vector3(transform.parent.right.x, 0, 0);
+        var _direction = transform.parent.InverseTransformPoint(_parentForwardPosition);
         var _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
         transform.localRotation = Quaternion.Euler(0f, 0f, _angle);
-    }
+        //LookAtPosition(_parentForwardPosition);
 
-    private void SetRotation(Quaternion _rotation)
-    {
-        transform.rotation = _rotation;
+        //var _direction = transform.parent.InverseTransformPoint(_target);
+        //var _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        //transform.localRotation = Quaternion.Euler(0f, 0f, _angle);
     }
 
     private bool CanRotate()
